568e5367d8310895c91efba52d685b6597cfdb4a
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-08 14:42:16 -0400

    styles in javascript and classList.add

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const produtos = document.querySelector(".produtos a");
 
 console.log(nav);
 console.log(produtos);
+
+console.log(produtos.href);
+
+produtos.remove();
+
+nav.style.backgroundColor = "black";
+nav.style.padding = "1rem";
+
+nav.classList.add("ativo");
