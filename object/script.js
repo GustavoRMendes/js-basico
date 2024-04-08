45cdf639577b85a0d23c1717640e1c420c578395
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-08 15:19:20 -0400

    object in javascript

---
+++ object/script.js
@@ -0,0 +1,10 @@
+const livro = {
+  nome : "Harry Potter" ,
+  ano : 2012 ,
+  autor : "Voldemort" ,
+};
+
+console.log(livro);
+console.log(livro.nome);
+console.log(livro.ano);
+console.log(livro.autor);
