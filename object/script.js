e264885dc5d0af865c4588df4ed21d534f5e28fb
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-08 15:28:35 -0400

    methods and property

--- object/script.js
+++ object/script.js
@@ -8,3 +8,8 @@ console.log(livro);
 console.log(livro.nome);
 console.log(livro.ano);
 console.log(livro.autor);
+console.log(livro.autor.length);
+console.log(livro.autor.toUpperCase());
+
+const decimal = 2981.10291;
+console.log(decimal.toFixed(2));
