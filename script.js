fb35e29bd911c626f771cdd67f14296907779e72
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-08 14:36:28 -0400

    add document.querySelector

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-const nome = 'JavaScript';
-console.log(nome);
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+
+console.log(nav);
+console.log(produtos);
