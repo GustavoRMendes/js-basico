d55baf8a8999c10b1024243447e2bbbdaa620500
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-08 14:55:10 -0400

    add math

---
+++ datas/script.js
@@ -0,0 +1,10 @@
+const text = "Javascript";
+const text2 = "Hello";
+console.log(text2 + " " + text);
+
+let total = 0;
+const compras = 30;
+const imposto = 10;
+total = compras + imposto;
+const dobro = total * 2;
+console.log(dobro);
