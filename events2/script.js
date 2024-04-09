2c3f26f7557749440d3d2c97d510bd509b886e14
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-09 10:10:27 -0400

    window events

--- events2/script.js
+++ events2/script.js
@@ -1,10 +1,27 @@
-const buttonAtivar = document.querySelector("button")
-function handleClick (event) {
-  buttonAtivar.innerHTML = "DESATIVADO"
-  buttonAtivar.classList.toggle("green")
-  if (buttonAtivar.classList.contains("green")){
-    buttonAtivar.innerHTML = "ATIVADO"
+const buttonAtivar = document.querySelector("button");
+function handleClick(event) {
+  buttonAtivar.innerHTML = "DESATIVADO";
+  buttonAtivar.classList.toggle("green");
+  if (buttonAtivar.classList.contains("green")) {
+    buttonAtivar.innerHTML = "ATIVADO";
   }
 }
 buttonAtivar.addEventListener("click", handleClick);
 
+// Window
+
+console.log(window);
+const larguraTela = window.innerWidth;
+
+function coordenadaMouse (event) {
+  const coordenadas = {
+    x: event.x , 
+    y: event.y ,
+  }
+  console.log(coordenadas);
+}
+function scroll () {
+  console.log(window.scrollY);
+}
+window.addEventListener("mousemove", coordenadaMouse);
+window.addEventListener("scroll",scroll);
