508c70bce47a20970aa1edf0e92712f10d65db28
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-09 14:56:26 -0400

    for

---
+++ arrays/script.js
@@ -0,0 +1,10 @@
+const list = ['Gustavo', 'Felipe', "Téo", "Camilo"];
+console.log(list);
+
+list.forEach(nomes => {
+    console.log(nomes);
+});
+
+for (let i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
