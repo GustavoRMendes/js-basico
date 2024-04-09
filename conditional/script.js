286a5d4bb6129e4a7bf9c2f09ad8921ed9c9c23e
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-09 11:14:38 -0400

    boolean conditional

---
+++ conditional/script.js
@@ -0,0 +1,25 @@
+if (true) {
+  console.log("teste");
+} else {
+  console.log("");
+}
+if (false) {
+  console.log("");
+} else {
+  console.log();
+}
+
+
+const p = document.querySelector("p")
+if (p) {
+  console.log("Existe a tag <p>");
+} else {
+  console.log("Não existe a tag <p>")
+}
+
+const a = document.querySelector("a")
+if (a) {
+  console.log("Existe a tag <a>")
+} else {
+  console.log("Não existe a tag <a>")
+}
