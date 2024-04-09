9c1ba6aea89692ed40d1ccf5ff12cee11d47cc3a
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-09 17:35:43 -0400

    Array Like(node list) and Array (Array.from)

--- arrays/script.js
+++ arrays/script.js
@@ -1,23 +1,34 @@
-const list = ['Gustavo', 'Felipe', "Téo", "Camilo"];
+const list = ["Gustavo", "Felipe", "Téo", "Camilo"];
 console.log(list);
 
-list.forEach(nomes => {
-    console.log(nomes);
+list.forEach((nomes) => {
+  console.log(nomes);
 });
 
 for (let i = 0; i < list.length; i++) {
   console.log(list[i]);
 }
-const numbers = [1, 2, 3,]
+const numbers = [1, 2, 3];
 
-numbers.forEach(n => {
+numbers.forEach((n) => {
   console.log(n);
-})
-
-
+});
+// ARRAY LIKE
 const links = document.querySelectorAll("nav a");
-console.log(links)
+console.log(links); // NODE LIST
 
-links.forEach(link => {
+// ARRAY
+// const links = Array.from(document.querySelectorAll("nav a"));
+
+
+links.forEach((link) => {
   console.log(link.href);
-})
+});
+
+const languages = ["Javascript", "C++", "PYTHON", "JAVA"];
+
+languages.push("C#");
+languages.pop();
+languages.shift();
+languages.unshift("Ruby");
+console.log(languages);
